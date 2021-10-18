9a2d474280b0e5ee518368f086745cd95db79468
syoch <hacksyoch@gmail.com>
2021-10-18 04:55:07 -0400

    update: ignore invite link

--- find_url.ts
+++ find_url.ts
@@ -1,5 +1,9 @@
 export function findUrls(text: string): string[] {
   const regex = /http(s?):\/\/[^\s]*/gm;
+  const ignore = /http(s?):\/\/discord\.(com\/invite|gg)\//gm;
+
   const matches = [...text.matchAll(regex)];
-  return matches.map(match => match[0]);
+  return matches
+    .map(match => match[0])
+    .filter(a => !a.match(ignore));
 }
