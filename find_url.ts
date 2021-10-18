13187f1394457e4cd0a6771c284bd95e1cdd06f6
syoch <hacksyoch@gmail.com>
2021-10-18 05:37:19 -0400

    update: ignore some pictures ext

--- find_url.ts
+++ find_url.ts
@@ -1,9 +1,12 @@
 export function findUrls(text: string): string[] {
   const regex = /http(s?):\/\/[^\s]*/gm;
   const ignore = /http(s?):\/\/discord\.(com\/invite|gg)\//gm;
-
+  const ignoredExtensions = [
+    "png", "gif", "jpg"
+  ];
   const matches = [...text.matchAll(regex)];
   return matches
     .map(match => match[0])
-    .filter(a => !a.match(ignore));
+    .filter(a => !a.match(ignore))
+    .filter(value => ignoredExtensions.some(ext => value.endsWith(ext)));
 }
