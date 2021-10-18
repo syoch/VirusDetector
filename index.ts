16cd8f1192c7cfdcf450d07abceaf0852f14e401
syoch <hacksyoch@gmail.com>
2021-10-18 04:00:13 -0400

    update: impl on_message handle

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { startBot } from "https://deno.land/x/discordeno/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { check } from "./checker.ts";
 
 startBot({
   token: config().DISCORD_TOKEN,
@@ -8,8 +9,25 @@ startBot({
     ready() {
       console.log("Successfully connected to gateway");
     },
-    messageCreate(message) {
-      console.log(message.content)
+    async messageCreate(message) {
+      const scanResult = await check(message);
+      let response = "";
+
+      response += "```md\n"
+      for (const key in scanResult) {
+        if (!Object.prototype.hasOwnProperty.call(scanResult, key)) {
+          continue
+        }
+        const result = scanResult[key];
+
+        if (result.length > 0) {
+          response += `${key}: ${result.join(", ")}\n`;
+        }
+      }
+      response += "```";
+
+      message.reply(response, false);
+
     },
   },
 });
